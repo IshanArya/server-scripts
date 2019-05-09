8a8bf721a59baca2c831d99894c7f932db7ed157
Ishan Arya <aryaishn@gmail.com>
2019-05-09 03:03:24 -0400

    take 3

--- radarr-download.sh
+++ radarr-download.sh
@@ -3,7 +3,9 @@
 script_name=$(basename -- "$0" | cut -f 1 -d '.')
 movie_folder_path="$radarr_movie_path"
 movie_parent_folder="$(basename "$movie_folder_path")"
+TO=$"media:\"Media/Movies/$movie_parent_folder\""
+
 
 echo "Uploading $movie_parent_folder to drive." >> ~/server-scripts/$script_name.log
-rclone copy $movie_folder_path media:"Media/Movies/$movie_parent_folder/"
+rclone copy $movie_folder_path $TO
 echo "Done uploading $movie_parent_folder to drive." >> ~/server-scripts/$script_name.log
