f0c3ad077dfb6ad6a8b9ed92b1ec696b18460f47
Ishan Arya <aryaishn@gmail.com>
2019-05-09 00:04:25 -0400

    Easier method

--- radarr-download.sh
+++ radarr-download.sh
@@ -2,13 +2,13 @@
 
 script_name=$(basename -- "$0" | cut -f 1 -d '.')
 # movie_path="$radarr_moviefile_path"
-movie_path="~/Files/Download/Iron Man (2007)/Iron Man (2007).mkv"
-movie_folder_path="$(dirname "$movie_path")/"
+# movie_path="~/Files/Download/Iron Man (2007)/Iron Man (2007).mkv"
+movie_folder_path="$radarr_movie_path"
 movie_parent_folder="$(basename $movie_folder_path)"
 
 # echo $movie_folder_path
 # echo $movie_parent_folder
 
 echo "Uploading $movie_parent_folder to drive." >> ~/projects/server-scripts/$script_name.log
-rclone copy $movie_folder_path media:"Media/Movies/$movie_parent_folder"
+# rclone copy $movie_folder_path media:"Media/Movies/$movie_parent_folder"
 echo "Done uploading $movie_parent_folder to drive." >> ~/projects/server-scripts/$script_name.log
