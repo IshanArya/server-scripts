0360dfb82db1c4eb42d01078267ae69ca1cc8179
Ishan Arya <aryaishn@gmail.com>
2019-05-09 02:48:24 -0400

    take 2

---
+++ old-notes.md
@@ -0,0 +1,2 @@
+movie_folder_path="$(dirname "$movie_path")/"
+movie_path="$radarr_moviefile_path"
--- radarr-download.sh
+++ radarr-download.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 script_name=$(basename -- "$0" | cut -f 1 -d '.')
-# movie_path="$radarr_moviefile_path"
-# movie_path="~/Files/Download/Iron Man (2007)/Iron Man (2007).mkv"
 movie_folder_path="$radarr_movie_path"
-movie_parent_folder="$(basename $movie_folder_path)"
+movie_parent_folder="$(basename "$movie_folder_path")"
 
-# echo $movie_folder_path
-# echo $movie_parent_folder
-
-echo "Uploading $movie_parent_folder to drive." >> ~/projects/server-scripts/$script_name.log
-# rclone copy $movie_folder_path media:"Media/Movies/$movie_parent_folder"
-echo "Done uploading $movie_parent_folder to drive." >> ~/projects/server-scripts/$script_name.log
+echo "Uploading $movie_parent_folder to drive." >> ~/server-scripts/$script_name.log
+rclone copy $movie_folder_path media:"Media/Movies/$movie_parent_folder/"
+echo "Done uploading $movie_parent_folder to drive." >> ~/server-scripts/$script_name.log
